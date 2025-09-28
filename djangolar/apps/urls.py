d452ba18b0ed06168b7ea81bb0fe7b071ea9f9d3
diasjakupov <dias.jakupov@zimran.io>
2025-09-28 16:32:01 +0500

    create home page + assosiated pages: users, city_time, counter

--- djangolar/apps/urls.py
+++ djangolar/apps/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', view = views.home, name='home'),
+    path('users/', view = views.users, name='users'),
+    path('city-time/', view = views.city_time, name='city_time'),
+    path('cnt/', view = views.counter, name='counter'),
 ]
